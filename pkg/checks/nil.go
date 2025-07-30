42e2c03f5b119d57a9f5cb1838917c9b586ddc46
Satish Chandra <satish.chandra@cashfree.com>
2025-07-30 11:44:19 +0530

    refactored code for more efficient scan

--- pkg/checks/nil.go
+++ pkg/checks/nil.go
@@ -8,100 +8,169 @@ import (
 	"github.com/devglyph1/panicscan/pkg/report"
 )
 
-// checkNilDereference performs basic nil pointer dereference detection
-// using simple pattern matching and basic data flow analysis.
+// env tracks, for each variable, whether it is definitely non-nil in the current context.
+type env map[string]bool
+
+// copy returns a shallow copy of an env map.
+func (e env) copy() env {
+	out := env{}
+	for k, v := range e {
+		out[k] = v
+	}
+	return out
+}
+
+// checkNilDereference traverses expressions and tracks nil-ness context.
 func checkNilDereference(fset *token.FileSet, root ast.Node, ti *types.Info, file string, rep *report.Report) {
-	// Track variables that are known to be nil or non-nil
-	nilVars := make(map[string]bool)    // true = definitely nil
-	nonNilVars := make(map[string]bool) // true = definitely non-nil
+	// entry: unknown nil-ness for everything
+	analyzeNode(root, env{}, fset, ti, file, rep)
+}
 
-	// First pass: collect nil assignments and checks
-	ast.Inspect(root, func(n ast.Node) bool {
-		switch node := n.(type) {
-		case *ast.AssignStmt:
-			// Look for explicit nil assignments: var x *T = nil
-			for i, lhs := range node.Lhs {
-				if i < len(node.Rhs) {
-					if ident, ok := lhs.(*ast.Ident); ok {
-						if isNilIdent(node.Rhs[i]) {
-							nilVars[ident.Name] = true
-							delete(nonNilVars, ident.Name)
-						} else if !isNilIdent(node.Rhs[i]) {
-							// Non-nil assignment
-							nonNilVars[ident.Name] = true
-							delete(nilVars, ident.Name)
-						}
-					}
-				}
-			}
-		case *ast.IfStmt:
-			// Look for nil checks: if x != nil { ... }
-			if bin, ok := node.Cond.(*ast.BinaryExpr); ok {
-				if ident, ok := bin.X.(*ast.Ident); ok && isNilIdent(bin.Y) {
-					switch bin.Op {
-					case token.NEQ: // x != nil
-						// Inside the if block, x is non-nil
-						ast.Inspect(node.Body, func(inner ast.Node) bool {
-							nonNilVars[ident.Name] = true
-							return true
-						})
-					case token.EQL: // x == nil
-						// Inside the if block, x is nil
-						ast.Inspect(node.Body, func(inner ast.Node) bool {
-							nilVars[ident.Name] = true
-							return true
-						})
-					}
-				}
-			}
+// analyzeNode recursively analyzes AST nodes with an environment specifying nil facts.
+func analyzeNode(node ast.Node, e env, fset *token.FileSet, ti *types.Info, file string, rep *report.Report) {
+	switch n := node.(type) {
+
+	case *ast.IfStmt:
+		// Analyze the condition for new info, then body with updated env.
+		postEnv := analyzeCond(n.Cond, e)
+		// then-branch: guaranteed facts
+		analyzeNode(n.Body, postEnv, fset, ti, file, rep)
+		if n.Else != nil {
+			// else-branch: use old env for now (could be improved)
+			analyzeNode(n.Else, e, fset, ti, file, rep)
 		}
-		return true
-	})
+		analyzeNode(n.Cond, e, fset, ti, file, rep)
+		return
 
-	// Second pass: check for potential nil dereferences
-	ast.Inspect(root, func(n ast.Node) bool {
-		switch node := n.(type) {
+	case *ast.BinaryExpr:
+		// Handle short-circuit for logic op
+		if n.Op == token.LAND || n.Op == token.LOR {
+			handleShortCircuitBool(n, e, fset, ti, file, rep)
+			return
+		}
+	}
+
+	// For general traversal.
+	ast.Inspect(node, func(child ast.Node) bool {
+		switch x := child.(type) {
+		case *ast.BinaryExpr:
+			if x.Op == token.LAND || x.Op == token.LOR {
+				handleShortCircuitBool(x, e, fset, ti, file, rep)
+				return false
+			}
 		case *ast.StarExpr:
-			// Direct dereference: *ptr
-			if ident, ok := node.X.(*ast.Ident); ok {
-				pos := fset.Position(node.Star)
-				if nilVars[ident.Name] {
-					rep.Error(file, pos.Line, pos.Column, "nil pointer dereference")
-				} else if !nonNilVars[ident.Name] {
-					// Unknown state - could be nil
-					rep.Warning(file, pos.Line, pos.Column, "possible nil pointer dereference")
+			if id, ok := x.X.(*ast.Ident); ok {
+				v := e[id.Name]
+				pos := fset.Position(x.Star)
+				if v {
+					return true // definitely non-nil
 				}
+				// Unknown; could be nil unless variable is always initialized
+				rep.Warning(file, pos.Line, pos.Column, "possible nil pointer dereference")
 			}
+
 		case *ast.SelectorExpr:
-			// Method call or field access: ptr.Method() or ptr.Field
-			if ident, ok := node.X.(*ast.Ident); ok {
-				// Check if this is a pointer type
-				if typ := ti.TypeOf(node.X); typ != nil {
-					if _, ok := typ.(*types.Pointer); ok {
-						pos := fset.Position(node.Pos())
-						if nilVars[ident.Name] {
-							rep.Error(file, pos.Line, pos.Column, "nil pointer dereference in field/method access")
-						} else if !nonNilVars[ident.Name] {
-							rep.Warning(file, pos.Line, pos.Column, "possible nil pointer dereference in field/method access")
-						}
+			// method/field: ptr.Method() or ptr.Field
+			if id, ok := x.X.(*ast.Ident); ok {
+				if _, ok := ti.TypeOf(x.X).(*types.Pointer); ok {
+					v := e[id.Name]
+					pos := fset.Position(x.Pos())
+					if v {
+						return true // non-nil
 					}
+					rep.Warning(file, pos.Line, pos.Column, "possible nil pointer dereference in field/method access")
 				}
 			}
+
 		case *ast.IndexExpr:
-			// Array/slice indexing: arr[i] (could be nil slice)
-			if ident, ok := node.X.(*ast.Ident); ok {
-				if typ := ti.TypeOf(node.X); typ != nil {
-					if _, ok := typ.(*types.Slice); ok {
-						pos := fset.Position(node.Lbrack)
-						if nilVars[ident.Name] {
-							rep.Error(file, pos.Line, pos.Column, "nil slice access")
-						} else if !nonNilVars[ident.Name] {
-							rep.Warning(file, pos.Line, pos.Column, "possible nil slice access")
-						}
+			if id, ok := x.X.(*ast.Ident); ok {
+				if _, ok := ti.TypeOf(x.X).(*types.Slice); ok {
+					v := e[id.Name]
+					pos := fset.Position(x.Lbrack)
+					if v {
+						return true
 					}
+					rep.Warning(file, pos.Line, pos.Column, "possible nil slice access")
 				}
 			}
 		}
 		return true
 	})
+}
+
+// handleShortCircuitBool analyzes short-circuit boolean logic with context propagation.
+func handleShortCircuitBool(be *ast.BinaryExpr, e env, fset *token.FileSet, ti *types.Info, file string, rep *report.Report) {
+	switch be.Op {
+	case token.LAND:
+		// For a && b:
+		// - left: in parent env
+		// - right: env updated with facts from left
+		analyzeNode(be.X, e, fset, ti, file, rep)
+		newEnv := analyzeCond(be.X, e)
+		analyzeNode(be.Y, newEnv, fset, ti, file, rep)
+	case token.LOR:
+		// For a || b:
+		// - left: in parent env
+		// - right: only relevant if left is false, so assume negation of left
+		analyzeNode(be.X, e, fset, ti, file, rep)
+		newEnv := analyzeCondNegated(be.X, e)
+		analyzeNode(be.Y, newEnv, fset, ti, file, rep)
+	}
+}
+
+// analyzeCond returns a new environment with facts inferred from cond under "true".
+func analyzeCond(expr ast.Expr, e env) env {
+	switch c := expr.(type) {
+	case *ast.BinaryExpr:
+		switch c.Op {
+		case token.NEQ:
+			if id, ok := c.X.(*ast.Ident); ok && isNilIdent(c.Y) {
+				ne := e.copy()
+				ne[id.Name] = true // definitely non-nil
+				return ne
+			}
+			if id, ok := c.Y.(*ast.Ident); ok && isNilIdent(c.X) {
+				ne := e.copy()
+				ne[id.Name] = true
+				return ne
+			}
+		case token.EQL:
+			if _, ok := c.X.(*ast.Ident); ok && isNilIdent(c.Y) {
+				// could add ne[id.Name] = false but we only track non-nil state for now
+				return e.copy()
+			}
+			if _, ok := c.Y.(*ast.Ident); ok && isNilIdent(c.X) {
+				return e.copy()
+			}
+		}
+	}
+	return e.copy()
+}
+
+// analyzeCondNegated returns a new env with facts inferred from cond under "false".
+func analyzeCondNegated(expr ast.Expr, e env) env {
+	switch c := expr.(type) {
+	case *ast.BinaryExpr:
+		switch c.Op {
+		case token.NEQ:
+			if _, ok := c.X.(*ast.Ident); ok && isNilIdent(c.Y) {
+				return e.copy()
+			}
+			if _, ok := c.Y.(*ast.Ident); ok && isNilIdent(c.X) {
+				return e.copy()
+			}
+		case token.EQL:
+			if id, ok := c.X.(*ast.Ident); ok && isNilIdent(c.Y) {
+				ne := e.copy()
+				ne[id.Name] = true // under !(x == nil), x is non-nil
+				return ne
+			}
+			if id, ok := c.Y.(*ast.Ident); ok && isNilIdent(c.X) {
+				ne := e.copy()
+				ne[id.Name] = true
+				return ne
+			}
+		}
+	}
+	return e.copy()
 }
